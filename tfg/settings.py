242b5ce1ebd48df22f10b1f4e315a982f3dd5f77
LauraIniesta <lauinirod11@gmail.com>
2023-04-27 19:26:37 +0200

    bootstrap

--- tfg/settings.py
+++ tfg/settings.py
@@ -151,8 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticHeroku')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL= "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 #DATABASE_URL = 'postgres://alumnodb:alumnodb@localhost:5432/psi'
