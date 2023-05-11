661681c5f6d3b5a6988b39a52ac0f267b623e4be
LauraIniesta <lauinirod11@gmail.com>
2023-05-11 13:02:49 +0200

    video prueba

--- tfg/settings.py
+++ tfg/settings.py
@@ -155,3 +155,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 MEDIA_URL= "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
+FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880 # 5 MB
